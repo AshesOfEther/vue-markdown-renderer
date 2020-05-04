cc940c5d45264b4bf4beb4216e1d40ed623f9ce1
Tobias SN <15670490+TobiasSN@users.noreply.github.com>
2020-05-04 16:54:53 +0200

    Fix bug where certain characters are replaced with HTML escape codes

--- src/MarkdownRenderer.js
+++ src/MarkdownRenderer.js
@@ -49,7 +49,9 @@ const componentMapping = {
 			))
 		]
 	),
-	text: ({token}) => token.text,
+	// We need to use the raw text, as it isn't sanitized for HTML. Using the sanitized
+	// text would cause weirdness like "Y&#39;all" instead of "Y'all".
+	text: ({token}) => token.raw,
 	paragraph: "paragraph",
 	// Inline
 	escape: ({token}) => token.text,
